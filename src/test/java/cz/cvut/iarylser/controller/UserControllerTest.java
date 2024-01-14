6eb01bd03258df0334eec4752707bae2255d1809
Strogolsky <Strogolsky@gmail.com>
2024-01-14 14:10:21 +0100

    add test for getAllUsers in UserControllerTest.java

--- src/test/java/cz/cvut/iarylser/controller/UserControllerTest.java
+++ src/test/java/cz/cvut/iarylser/controller/UserControllerTest.java
@@ -1,22 +1,66 @@
 package cz.cvut.iarylser.controller;
 
+import cz.cvut.iarylser.dao.DTO.UserDTO;
+import cz.cvut.iarylser.dao.entity.User;
+import cz.cvut.iarylser.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@WebMvcTest(UserController.class)
 class UserControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private UserService userService;
 
     @BeforeEach
     void setUp() {
+        AutoCloseable autoCloseable = MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    void getAllUsers() {
+    void getAllUsers() throws Exception {
+        User user1 = new User();
+        User user2 = new User();
+        user1.setNickname("user1");
+        user2.setNickname("user2");
+        List<User> users = Arrays.asList(user1, user2);
+
+        UserDTO userDTO1 = new UserDTO();
+        UserDTO userDTO2 = new UserDTO();
+        userDTO1.setNickname("user1");
+        userDTO2.setNickname("user2");
+        List<UserDTO> userDTOs = Arrays.asList(userDTO1, userDTO2);
+
+        when(userService.getAllUsers()).thenReturn(users);
+        when(userService.convertToDTOList(users)).thenReturn(userDTOs);
+
+        mockMvc.perform(get("/user"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].nickname").value(user1.getNickname()))
+                .andExpect(jsonPath("$[1].nickname").value(user2.getNickname()));
     }
 
     @Test
     void getUserById() {
+
     }
 
     @Test
