d4f90a25a9cfe9f2a7fcc0c3d7f6b0647100f697
Strogolsky <Strogolsky@gmail.com>
2024-01-14 18:10:40 +0100

    add test for updateCapacity getAvailableSeat in EventTest

---
+++ src/test/java/cz/cvut/iarylser/dao/entity/EventTest.java
@@ -0,0 +1,28 @@
+package cz.cvut.iarylser.dao.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventTest {
+
+
+
+    @Test
+    void getAvailableSeat() {
+        Event event = new Event();
+        event.setSoldTickets(15);
+        event.setCapacity(20);
+
+        assertEquals(event.getAvailableSeat(), 5);
+    }
+
+    @Test
+    void updateCapacity() {
+        Event event = new Event();
+        event.setCapacity(10);
+        event.setSoldTickets(8);
+
+        assertTrue(event.updateCapacity(9));
+    }
+}
