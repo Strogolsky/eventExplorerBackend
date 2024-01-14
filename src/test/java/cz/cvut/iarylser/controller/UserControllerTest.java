fc082296dbd3cbc71fe9ade27a0bda3b9798a4c9
Strogolsky <Strogolsky@gmail.com>
2024-01-14 16:07:51 +0100

    add test for createUser in UserControllerTest.java

--- src/test/java/cz/cvut/iarylser/controller/UserControllerTest.java
+++ src/test/java/cz/cvut/iarylser/controller/UserControllerTest.java
@@ -1,14 +1,17 @@
 package cz.cvut.iarylser.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import cz.cvut.iarylser.dao.DTO.UserDTO;
 import cz.cvut.iarylser.dao.entity.User;
 import cz.cvut.iarylser.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -79,7 +82,43 @@ class UserControllerTest {
     }
 
     @Test
-    void createUser() {
+    void createUser() throws Exception{
+        User user = new User();
+        user.setNickname("testUser");
+        user.setAge(25);
+        user.setEmail("test@example.com");
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        user.setDescription("Test description");
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setNickname(user.getNickname());
+        userDTO.setAge(user.getAge());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setFirstName(user.getFirstName());
+        userDTO.setLastName(user.getLastName());
+        userDTO.setDescription(user.getDescription());
+
+        when(userService.createUser(Mockito.any(User.class))).thenReturn(user);
+        when(userService.convertToDTO(Mockito.any(User.class))).thenReturn(userDTO);
+
+
+        user.setNickname("a;sdasd");
+
+        String userJson = new ObjectMapper().writeValueAsString(user);
+
+
+
+        mockMvc.perform(post("/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nickname").value(userDTO.getNickname()))
+                .andExpect(jsonPath("$.age").value(userDTO.getAge()))
+                .andExpect(jsonPath("$.email").value(userDTO.getEmail()))
+                .andExpect(jsonPath("$.firstName").value(userDTO.getFirstName()))
+                .andExpect(jsonPath("$.lastName").value(userDTO.getLastName()))
+                .andExpect(jsonPath("$.description").value(userDTO.getDescription()));
     }
 
     @Test
