13f70bd79de8cb03b289f267e8dd3a873198c093
Strogolsky <Strogolsky@gmail.com>
2024-07-19 12:42:45 +0200

    refactor #1: add .gitignore

---
+++ src/test/java/cz/cvut/iarylser/dao/mappersDTO/EventMapperDTOTest.java
@@ -0,0 +1,20 @@
+package cz.cvut.iarylser.dao.mappersDTO;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventMapperDTOTest {
+
+    @Test
+    void toDTO() {
+    }
+
+    @Test
+    void toDTOList() {
+    }
+
+    @Test
+    void toEntity() {
+    }
+}
