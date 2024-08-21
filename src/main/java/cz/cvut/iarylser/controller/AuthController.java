913b61d270a919d25e08ac537c625c89e44147f3
Strogolsky <Strogolsky@gmail.com>
2024-08-21 18:24:19 +0300

    feat #4: add documentation

--- src/main/java/cz/cvut/iarylser/controller/AuthController.java
+++ src/main/java/cz/cvut/iarylser/controller/AuthController.java
@@ -1,9 +1,10 @@
 package cz.cvut.iarylser.controller;
-
 import cz.cvut.iarylser.dao.DTO.JwtAuthenticationResponse;
 import cz.cvut.iarylser.dao.DTO.SignInRequest;
 import cz.cvut.iarylser.dao.DTO.SignUpRequest;
 import cz.cvut.iarylser.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,12 +21,18 @@ public class AuthController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/sign-up")
+    @Operation(summary = "Sign up")
+    @ApiResponse(responseCode = "200", description = "Successful sign up")
+    @ApiResponse(responseCode = "403", description = "This user already exists")
     public JwtAuthenticationResponse signUp(@RequestBody @Valid SignUpRequest request) {
         log.info("POST request for sign up");
         return authenticationService.signUp(request);
     }
 
     @PostMapping("/sign-in")
+    @Operation(summary = "Sign in")
+    @ApiResponse(responseCode = "200", description = "Successful sign in")
+    @ApiResponse(responseCode = "403", description = "Incorrect username or password")
     public JwtAuthenticationResponse signIn(@RequestBody @Valid SignInRequest request) {
         log.info("POST request for sign in");
         return authenticationService.signIn(request);
