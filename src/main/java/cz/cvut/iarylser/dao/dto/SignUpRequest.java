73d1154652521af11c81ccaa38dbab308896495e
Strogolsky <Strogolsky@gmail.com>
2024-09-16 16:33:02 +0200

    feat #16: fix low code style

--- src/main/java/cz/cvut/iarylser/dao/dto/SignUpRequest.java
+++ src/main/java/cz/cvut/iarylser/dao/dto/SignUpRequest.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import org.hibernate.validator.constraints.Length;
 
 @Data
